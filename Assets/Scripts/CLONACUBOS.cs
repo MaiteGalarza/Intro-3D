8ccb5a11c4857338b21ddbbcceebc4ef6e2a1e93
MaiteGalarza <49189070@est.ort.edu.ar>
2025-05-15 12:12:01 -0300

    Salió a medias

--- Assets/Scripts/CLONACUBOS.cs
+++ Assets/Scripts/CLONACUBOS.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class CLONACUBOS : MonoBehaviour
 {
     public GameObject cuboPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(cuboPrefab);
+      
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ClonarCubo()
     {
-        
+        Instantiate(cuboPrefab);
     }
 }
